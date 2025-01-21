928a341a6ad299097cb09a91367d979066fa3df3
tristanlatr <19967168+tristanlatr@users.noreply.github.com>
2025-01-20 20:35:53 -0500

    Fix erroneous base class for the standard library use-def tests

--- tests/test_chains.py
+++ tests/test_chains.py
@@ -1414,6 +1414,6 @@ class TestUseDefChains(TestCase):
         code = "from foo import bar; bar(1, 2)"
         self.checkChains(code, "<Call> <- {<Constant>, <Constant>, bar}, bar <- {bar}")
 
-class TestUseDefChainsStdlib(TestDefUseChains):
+class TestUseDefChainsStdlib(TestUseDefChains):
     ast = _ast
 
