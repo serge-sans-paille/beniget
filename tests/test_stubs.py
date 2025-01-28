32e495303bc9df20fca1998003ab0e0467fe5054
tristanlatr <tristanlatr@users.noreply.github.com>
2025-01-27 20:18:12 -0500

    Skipping test_all_stubs() if the environment doesn't contain the GITHUB_USER variable

--- tests/test_stubs.py
+++ tests/test_stubs.py
@@ -2,6 +2,7 @@ from unittest import TestCase, skipIf
 import sys
 import ast as _ast
 import gast as _gast
+import os
 
 from .test_chains import StrictDefUseChains
 
@@ -26,7 +27,8 @@ class TestStubs(TestCase):
         for chains in c._builtins.values():
             assert len(chains.users())==0, chains
     
-    @skipIf(sys.version_info < (3, 8), reason='positional only syntax is used')
+    @skipIf((sys.version_info < (3, 8)) or ('GITHUB_USER' not in os.environ.keys()), 
+            reason='positional only syntax is used; only running this test on Github CI to make local development faster')
     def test_all_stubs(self):
         """
         This tests that we can parse all typeshed stubs and that beniget detect no errors.
