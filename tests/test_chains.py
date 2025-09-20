a6f7351e51b43713913d0347d3f3e395e3a13a81
tristanlatr <19967168+tristanlatr@users.noreply.github.com>
2025-09-20 20:35:05 +0000

    add a test

--- tests/test_chains.py
+++ tests/test_chains.py
@@ -343,7 +343,7 @@ while done:
         self.checkChains(code, ["f -> (f -> ())",  "f -> ()"])
     
     def test_try_except_refdef_exception_same_name(self):
-        code = 'try: ...\nexcept Exception as f: f=f\nf' # currently fails the sanity checks
+        code = 'try: ...\nexcept Exception as f: f=f\nf'
         self.check_message(code, ["W: unbound identifier 'f' at <unknown>:3:0"])
     
     def test_try_except_refdef_exception(self):
@@ -353,6 +353,11 @@ while done:
     def test_try_except_delete_exception(self):
         code = 'try: ...\nexcept Exception as e: f=e; del e\nf'
         self.checkChains(code, ['e -> (e -> (), e -> ())', 'f -> (f -> ())'])
+    
+    def test_try_multiple_excepts(self):
+        code = 'try:... \nexcept KeyError as e1: e2 \nexcept ValueError as e2: e1'
+        self.check_message(code, ["W: unbound identifier 'e2' at <unknown>:2:23", 
+            "W: unbound identifier 'e1' at <unknown>:3:25"])
     
     def test_try_except_delete_exception_in_finally(self):
         code = 'try: ...\nexcept Exception as e: f=e \nfinally: del e,f\nf'
